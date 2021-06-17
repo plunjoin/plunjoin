d8e03135add9a9e7d4d14889444e6805b4948aaf
plunjoin <plunjoin@gmail.com>
2021-06-17 15:30:41 +0800

    测试

--- app/controller/home.js
+++ app/controller/home.js
@@ -1,11 +1,28 @@
 'use strict';
 
 const Controller = require('egg').Controller;
+const crypto = require('crypto');
+const md5 = crypto.createHash('md5');
+const token = "zwuwBmGwKWh2U9I37KNKxefTtzPUn342"
 
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
-    ctx.body = 'hi, egg';
+    console.log(md5);
+    var timestamp = new Date().getTime();
+    var md5token = md5.update(timestamp + '' + md5.update(token));
+    let res = await ctx.curl("http://45.136.244.36:8888/system?action=GetSystemTotal", {
+      method: "POST",
+      headers: {
+        cookie: './' + md5.update('http://45.136.244.36:8888') + '.cookie'
+      },
+      data: {
+        request_time: timestamp,
+        request_token: md5token,
+      }
+    })
+    console.log(res);
+    ctx.body = res;
   }
 }
 
